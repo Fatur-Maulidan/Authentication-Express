04caac388fa4e6b3b1d66d26b95a583cfa00de93
Fatur Maulidan <mfatur757@gmail.com>
2024-09-10 05:35:50 +0700

    fix : fix validation

--- validations/userValidations.js
+++ validations/userValidations.js
@@ -10,12 +10,7 @@ const validationAuthData = [
 
 const validationUserData = [
     body('email').trim().normalizeEmail().notEmpty().withMessage('Email is required').bail()
-        .isEmail().withMessage('Email is not valid').custom(async (email) => {
-            const existingUser = await userModel.getUserByEmail(email);
-            if(existingUser) {
-                return Promise.reject('Email already in use');
-            }
-        }),
+        .isEmail().withMessage('Email is not valid'),
     body('password').notEmpty().withMessage('Password is required').bail()
         .isLength({min: 8}).withMessage('Password must be at least 8 characters long'),
 ]
